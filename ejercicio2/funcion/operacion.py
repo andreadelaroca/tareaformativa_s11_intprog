03e10d5cc4163d510b57b890cf5f0784be3e29de
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-06-09 09:29:19 -0600

    Utilicé un bucle while para describir el proceso de división del decimal hasta terminar en un resuido 0

--- ejercicio2/funcion/operacion.py
+++ ejercicio2/funcion/operacion.py
@@ -2,4 +2,9 @@ def decimal_binario(num):
     if num < 0:
         print("El número debe ser un entero positivo.")
         return None
-    
+    while num > 0:
+        binario = num % 2
+        num = num // 2
+    print(f"\nEl número en binario es {binario}")
+    return binario
+
