9c1f67b3d31c2729ce4677b919416b706bb326f0
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-06-09 09:44:28 -0600

    Corregí un error en la función, añadí un str vacío para binario

--- ejercicio2/funcion/operacion.py
+++ ejercicio2/funcion/operacion.py
@@ -1,10 +1,10 @@
 def decimal_binario(num):
+    binario = "" #str vacío para almacenar binario
     if num < 0:
         print("El número debe ser un entero positivo.")
         return None
     while num > 0:
-        binario = num % 2
-        
+        binario = str(num % 2) + binario
+        n //= 2
     print(f"\nEl número en binario es {binario}")
     return binario
-
