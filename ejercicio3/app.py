5dd6260c04a4d39c20039f51d242e9c8133ebb86
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-06-09 09:17:16 -0600

    Importé la función en el archivo app.py y pedí input de los valores al usuario

---
+++ ejercicio3/app.py
@@ -0,0 +1,6 @@
+import funcion.operacion as op
+
+numero = int(input("Ingrese un número entero positivo: "))
+exponente = int(input("Ingrese un exponente entero positivo: "))
+
+op.potencia_numero_exponente(numero, exponente)
